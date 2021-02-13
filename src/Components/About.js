2f978442e94d79162cd02ea47ede5562f0d606ea
Brooke Miller <brookecm28@gmail.com>
2021-02-12 21:48:14 -0700

    About section content

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='about'>About</div>
+        <div className='about'>
+            <div>
+                <p>After growing up and living in small-town Indiana for the first 18 years of my life, I took my first step in finding who I truly was by attending Purdue University.
+                    The four years I spent there studying also gave me the opportunity to truly discover who I was: someone who wanted more. By the end of my studies, I determined
+                    that I needed to get away, and I found myself in Colorado.
+                </p>
+                <p>
+                    I've lived in Colorado for three and a half years now. At the end of three years working for a tax debt resolution firm, I decided that I needed to do something more
+                    challenging than the work I was currently doing. I dabbled in computer programming in high school, but had not pursued it further after graduation. 
+                    Quarantine gave me the opportunity to do so, and I found myself taking a free online class for coding in my spare time.
+                </p>
+                <p>
+                    A little taste was all it took for me to fall in love with coding. I felt like I'd finally discovered what I was meant to do. So, I took the plunge and enrolled in
+                    DevMountain's Web Developer program. After 13 simultaneously long and short weeks, I've graduated and am ready to show the world what I'm truly made of.
+                </p>
+            </div>
+        </div>
     )
 }
 
