d1750df1624ca9d5fd1ef639b4c9af44a6917730
Brooke Miller <brookecm28@gmail.com>
2021-02-20 13:05:10 -0700

    about me section further responsiveness

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,9 +7,9 @@ function Contact() {
                 <div className='project-title'>Find me at the following:</div>
                 <div className='info-links'>
                     <p><a className='project-link' href='https://www.linkedin.com/in/brooke-miller-95a08479' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
-                    <p className='project-link'>|</p>
+                    <p className='project-link separator'>|</p>
                     <p><a className='project-link' href='https://github.com/brookecm28' target='_blank' rel='noopener noreferrer'>GitHub</a></p>
-                    <p className='project-link'>|</p>
+                    <p className='project-link separator'>|</p>
                     <p><a className='project-link' href='https://drive.google.com/file/d/1AmSyP1Mkzj3n6FjnzLiLLTWV9zuPI2Md/view?usp=sharing' target='_blank' rel='noopener noreferrer'>Resume</a></p>
                 </div>
                 <div className='contact-info project-title'>Email:</div>
