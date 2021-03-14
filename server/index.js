f505577f9ccb72d9404bb1eb9559b2e6c4ebdd2b
Brooke Miller <brookecm28@gmail.com>
2021-03-14 12:40:44 -0600

    createdd server for hosting

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const {SERVER_PORT} = process.env
+
+const app = express()
+
+app.use(express.static(`${__dirname}/../build`))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVERS_PORT}`)
+})
