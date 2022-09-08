3e6554ec0a8f2ba4cd8d232b65b79dd4e55a5d2e
brookecm28 <brookecm28@gmail.com>
2022-09-08 16:14:43 -0600

    Add contact info to data.json and map it

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import {contact} from '../data.json'
 
 function Contact() {
+    function loopContactInfo() {
+        return contact.map(item => {
+            return (
+                <div>
+                    <a href={`${item.url}`}>{item.name}</a>
+                </div>
+            )
+        })
+    }
+
     return (
         <div>
             <div>
-                <div>Find Me at the Following:</div>
+                <h2>Find Me at the Following:</h2>
                 <div>
                     <p><a href='https://www.linkedin.com/in/brooke-miller-95a08479' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
-                    <p>|</p>
+
                     <p><a href='https://github.com/brookecm28' target='_blank' rel='noopener noreferrer'>GitHub</a></p>
-                    <p>|</p>
+
                     <p><a href='https://drive.google.com/file/d/1AmSyP1Mkzj3n6FjnzLiLLTWV9zuPI2Md/view?usp=sharing' target='_blank' rel='noopener noreferrer'>Resume</a></p>
                 </div>
                 <div>Email:</div>
                 <div>
                     <a href='mailto:brooke.miller.dev@gmail.com'>brooke.miller.dev@gmail.com</a>
                 </div>
+                {loopContactInfo()}
             </div>
         </div>
     )
