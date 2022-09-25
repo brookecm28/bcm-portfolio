c0cb8b4078b5b5710bd3e84cea3adca8729aed40
Brooke Miller <brookecm28@gmail.com>
2022-09-25 09:17:22 -0600

    various styling updates, attempt to implement logo source

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import lobster from '../assets/icons/blue-lobster-logo.png'
-import lemon from '../assets/icons/lemon_prop.png'
+import lobster from '../assets/icons/richRecipes.png'
+import lemon from '../assets/icons/lemonProp.png'
 import { projects } from '../data.json'
 
 function Projects() {
     function loopProjects() {
         const htmlForReturn = []
         for (const key in projects) {
-            console.log('key', key)
+            const logo = projects[key].logo || `../assets/icons/${key}`
             htmlForReturn.push(
                 <div className='flex justify-center border-2 dark:border-secondary-light w-2/3 h-64'>
-                    <div className='w-1/2 border-r-2 dark:border-r-secondary-light dark:bg-secondary-light'>
-                        <h3>{projects[key].displayName}</h3>
+                    <div className='w-1/2 flex flex-col justify-around border-r-2 dark:border-r-secondary-light bg-secondary-light text-primary'>
+                        <h3 className='text-2xl'>{projects[key].displayName}</h3>
                         <p className='italic'>{projects[key].tagline}</p>
+                        <img src={logo} alt={key} />
                     </div>
-                    <div className='w-1/2 dark:bg-secondary-dark'>
+                    <div className='w-1/2 flex flex-col justify-center gap-8 items-center bg-primary dark:bg-secondary-dark'>
                         <button role="button" href={projects[key].source} target='_blank' rel='noopener noreferrer'
                             className='border-2 border-secondary-dark dark:border-primary h-12 w-48 hover:dark:bg-primary hover:dark:text-secondary-dark'>
                             Visit Site
